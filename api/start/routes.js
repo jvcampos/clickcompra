e3e32c6616f9327b2def09e4b0e2e4ba3c1a5db8
jvcampos <jjvcamposs@gmail.com>
2019-03-12 21:23:12 -0300

    feat: create category

--- api/start/routes.js
+++ api/start/routes.js
@@ -3,8 +3,13 @@
 const Route = use('Route')
 
 Route.group(() => {
+  //TODO fazer alterar
   Route.post('supermarket', 'SupermarketController.create');
+  //TODO fazer alterar
+  Route.post('manager', 'ManagerController.create');
+ //TODO Fazer o alterar
+  Route.post('category', 'CategoryController.create');
 
-  Route.post('manager', 'ManagerController.create')
+
 })
  .prefix('api');
