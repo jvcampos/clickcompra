b5b6e2e08b5d47e615576ea9de805c6e83010e8b
jvcampos <jjvcamposs@gmail.com>
2019-03-15 20:49:56 -0300

    feat: login session

--- api/app/Models/Manager.js
+++ api/app/Models/Manager.js
@@ -1,9 +1,20 @@
 'use strict'
 
 const Model = use('Model')
+const Hash = use('Hash')
 
 class Manager extends Model {
-  
+  static boot () {
+    super.boot()
+    this.addHook('beforeSave', async (userInstance) => {
+      if (userInstance.dirty.password) {
+        userInstance.password = await Hash.make(userInstance.password)
+      }
+    })
+  }
+  tokens () {
+    return this.hasMany('App/Models/Token')
+  }
 }
 
 module.exports = Manager
--- api/database/migrations/1551997614676_supermarket_schema.js
+++ api/database/migrations/1551997614676_supermarket_schema.js
@@ -5,6 +5,12 @@ const Schema = use('Schema')
 class SupermarketSchema extends Schema {
   up () {
     this.create('supermarkets', (table) => {
+      table
+        .integer('id_manager')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .notNullable()
       table.string('cnpj', 20).notNullable().unique()
       table.string('name', 80).notNullable().unique()
       table.string('address',150).notNullable()
@@ -17,6 +23,10 @@ class SupermarketSchema extends Schema {
 
   down () {
     this.drop('supermarkets')
+  }
+
+  user () {
+    return this.hasMany('App/Models/User')
   }
 }
 
