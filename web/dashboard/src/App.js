5a6685186f86e3698bf5c59e69d03f50d8ceadde
jvcampos <jjvcamposs@gmail.com>
2019-03-27 06:36:39 -0300

    refact: configuration redux

--- web/dashboard/src/App.js
+++ web/dashboard/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+
+import store from './store'
 
 import Login from "./components/login/login"
+// import MenuSuperior from './components/dashboard/menusuperior/menusuperior'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Login />
+        <Provider store={store}>
+          <Login />
+        </Provider>
       </div>
     );
   }
