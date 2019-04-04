c81b9888bab7a7d5a826ff0f35ea7911daf61a4a
jvcampos <jjvcamposs@gmail.com>
2019-04-04 09:55:34 -0300

    feature: done page login, with table

--- web/src/index.js
+++ web/src/index.js
@@ -4,4 +4,6 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css'
 import 'react-semantic-toasts/styles/react-semantic-alert.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <App />,
+document.getElementById('root'));
