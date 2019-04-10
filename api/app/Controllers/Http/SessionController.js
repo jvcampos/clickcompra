587c2dd917e655978acf20a48f72e7eb36f26b5d
jvcampos <jjvcamposs@gmail.com>
2019-04-10 18:31:12 -0300

    refact: done login page with aproved status

--- api/app/Controllers/Http/SessionController.js
+++ api/app/Controllers/Http/SessionController.js
@@ -1,14 +1,18 @@
 'use strict'
 const UserModel = use('App/Models/User');
+const SupermarketModel = use('App/Models/Supermarket');
+
 class SessionController {
   async createSession({ request, auth }) {
     const { email, password } = request.all()
     const token = await auth.attempt(email, password)
     const userData = await UserModel.findBy('email', email)
-    const { role } = userData
+    const { id, role } = userData
+    const { status } = await SupermarketModel.find(id)
     return {
       token,
-      role
+      role,
+      status_register: status
     }
   }
 }
