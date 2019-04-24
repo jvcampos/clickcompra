4b43c7725d7c572ad42706a6b54f9b7620d513f3
jvcampos <jjvcamposs@gmail.com>
2019-04-24 18:57:38 -0300

    refact: done all about page categories

--- web/src/components/categories/TableCategories.js
+++ web/src/components/categories/TableCategories.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Modal, Button, Icon, Header } from 'semantic-ui-react'
+import { Form, Popup, Table, Button, Modal, Icon, Header } from 'semantic-ui-react'
 import { SemanticToastContainer, toast } from 'react-semantic-toasts'
 
 import { bindActionCreators } from 'redux';
@@ -11,6 +11,44 @@ import './categories.css'
 class TableCategories extends Component {
   state = {
     statusModalRemove : false,
+    statusModalEdit: false,
+    name_categorie: this.props.data.name_categorie,
+    description: this.props.data.description,
+  }
+
+  updateCategorie = (id) => {
+    this.props.updateCategory(id, this.state.name_categorie, this.state.description)
+    this.props.onUpdateCategory()
+    this.showMessage('success', 'edit', 'Categoria alterada com sucesso !')
+    this.closeModalEdit()
+  }
+
+  showMessage(type, icon, title) {
+    setTimeout(() => {
+      toast(
+        {
+          type,
+          icon,
+          animation: 'bounce',
+          title,
+        },
+      );
+    }, 1000);
+  }
+
+
+  onHandleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  openModalEdit = () => {
+    this.setState({ statusModalEdit: true })
+  }
+
+  closeModalEdit = () => {
+    this.setState({ statusModalEdit: false })
   }
 
   openModalRemove = () => {
@@ -28,16 +66,65 @@ class TableCategories extends Component {
         <Table.Cell>100</Table.Cell>
         <Table.Cell>{this.props.data.description}</Table.Cell>
         <Table.Cell textAlign="center">
-            <Modal
-            basic size='small'
-            dimmer="blurring"
-            open={this.state.statusModalRemove}
+          <Modal
+            open={this.state.statusModalEdit}
+            className="modal_dados_gerente"
+            size="mini"
             trigger={
-              <Button onClick={this.openModalRemove} size="small" color="red" animated='fade'>
-                <Button.Content visible>EXCLUIR</Button.Content>
-                <Button.Content hidden><Icon name='close'/></Button.Content>
-              </Button>}
+              <Popup
+                content='Alterar categoria'
+                trigger={
+                  <Button onClick={this.openModalEdit} color="blue" icon>
+                    <Icon name='edit'/>
+                  </Button>
+                }
+              />
+            }
             >
+              <Modal.Header style={{ textAlign: 'center' }}>ALTERAR CATEGORIA</Modal.Header>
+              <Modal.Content>
+                <Modal.Description>
+                  <Header as='h3'>NOME</Header>
+                  <Form.Input
+                    onChange={this.onHandleChange}
+                    value={this.state.name_categorie}
+                    name="name_categorie"
+                    fluid icon='tag' iconPosition='left'
+                    placeholder='NOME' />
+                  <Header as='h3'>DESCRIÇÃO</Header>
+                  <Form.TextArea style={{
+                    width: '318px',
+                    maxWidth: '318px',
+                    height: '68px',
+                    maxHeight: '100px',
+                    margin: '0px',
+                    borderRadius: '6px',
+                    borderColor: '#c1bfbfbd',
+                  }}
+                    onChange={this.onHandleChange}
+                    value={this.state.description}
+                    name="description"
+                    rows={3}
+                    placeholder="Descrição da categoria"></Form.TextArea>
+                </Modal.Description>
+                <Modal.Actions style={{ marginTop: '10px' }}>
+                  <Button negative icon='close' onClick={this.closeModalEdit} labelPosition='right' content="Cancelar"></Button>
+                  <Button positive icon='checkmark'
+                    loading={this.state.loadingAddCategorie}
+                    onClick={() => this.updateCategorie(this.props.data.id)} labelPosition='right' content='Alterar' />
+                </Modal.Actions>
+              </Modal.Content>
+            </Modal>
+            <Modal
+              basic size='small'
+              dimmer="blurring"
+              open={this.state.statusModalRemove}
+              trigger={
+                <Button onClick={this.openModalRemove} size="small" color="red" animated='fade'>
+                  <Button.Content visible>EXCLUIR</Button.Content>
+                  <Button.Content hidden><Icon name='close'/></Button.Content>
+                </Button>}
+              >
               <Header icon='close' content='Excluir Categoria' />
               <Modal.Content>
                 <p>Você realmente deseja excluir a categoria selecionada ?</p>
