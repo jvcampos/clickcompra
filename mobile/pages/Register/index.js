dece1dcef7639baa5d674d906c3e4382d5046158
gabrielgsv <gabriel.soares.vilela@gmail.com>
2019-08-10 15:31:45 -0300

    update: add react hook and onChangeText

--- mobile/pages/Register/index.js
+++ mobile/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   View,
   Text
@@ -11,32 +11,59 @@ import { withNavigation } from 'react-navigation'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import styles from './styles'
 
-const Register = ({ navigation }) => (
+const Register = ({ navigation }) => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    cpf: '',
+    address: ''
+  })
+
+  return (
   <KeyboardAwareScrollView>
     <TextInput
       label='Nome'
       mode='outlined'
+      value={user.name}
       style={styles.input}
+      onChangeText={text => {
+        setUser({name: text})
+      }}
     />
     <TextInput
       label='Email'
       mode='outlined'
       style={styles.input}
+      onChangeText={text => {
+        setUser({email: text})
+      }}
     />
     <TextInput
+      secureTextEntry={true}
       label='Senha'
       mode='outlined'
+      type='password'
       style={styles.input}
+      onChangeText={text => {
+        setUser({password: text})
+      }}
     />
     <TextInput
       label='Cpf'
       mode='outlined'
       style={styles.input}
+      onChangeText={text => {
+        setUser({cpf: text})
+      }}
     />
     <TextInput
       label='Endereço'
       mode='outlined'
       style={styles.input}
+      onChangeText={text => {
+        setUser({address: text})
+      }}
     />
     <View style={styles.container}>
       <Button
@@ -47,6 +74,7 @@ const Register = ({ navigation }) => (
     </Button>
     </View>
   </KeyboardAwareScrollView>
-)
+  )
+}
 
 export default withNavigation(Register)
