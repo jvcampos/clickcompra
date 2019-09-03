9a837e2bf685c250541b676a53720de7bd0862c7
Vinicius <vinicius@ensemble.com.br>
2019-09-02 21:56:20 -0300

    feat: Add total inside the cart component and the button Buy

--- mobile/pages/Cart/index.js
+++ mobile/pages/Cart/index.js
@@ -1,46 +1,85 @@
 import React, { useRef, useState } from 'react'
-import { Text, View, Image, StyleSheet, ScrollView } from 'react-native'
+import { Text, View, Image, StyleSheet, ScrollView, FlatList } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { Button } from 'react-native-paper';
 
-const Cart = ({ navigation }) => {
+const CartProduct = ( { product } ) => {
+    return (
+        <View style={{ marginBottom: 20 }}>
+            <View style={styles.productCard}>
+                <View style={styles.imagemProductCard}>
+                    <Image
+                        style={styles.imageCard}
+                        source={require( '../../assets/legumes_categorie.png' )}
+                    />
+                </View>
+                <View style={styles.containerTextProductCard}>
+                    <Text style={styles.textProductCard}>
+                        {product.description}
+                    </Text>
+                    <Text style={styles.textPriceProduct}>
+                        R$ {product.value}
+                    </Text>
+                    <View style={styles.containerTotalQtd}>
+                        <Text style={styles.textTotalQtd}>{product.qtd}</Text>
+                    </View>
+                    <View style={styles.containerNameProduct}>
+                        <Text>{product.name}</Text>
+                    </View>
+                </View>
+                <View style={styles.containerButtonAndValue}>
+                    <View style={styles.buttonsRemoveProduct}>
+                        <View>
+                            <Icon name="trash" size={25} color={'#e74c3c'} />
+                        </View>
+                    </View>
+                    <View style={styles.containerValueTotalProduct}>
+                        <Text style={styles.textValueTotalProduct}>R$ {product.valueTotal}</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const Cart = ( { navigation } ) => {
 
     const allProducts = [
         {
-            index: 1,
-            name: 'Produto 01',
+            key: 1,
+            title: 'Produto 01',
             description: 'Descrição produto 01',
             value: 10,
             qtd: 2,
             valueTotal: 20
         },
         {
-            index: 2,
-            name: 'Produto 02',
+            key: 2,
+            title: 'Produto 02',
             description: 'Descrição produto 02',
             value: 20,
             qtd: 1,
             valueTotal: 20
         },
         {
-            index: 3,
-            name: 'Produto 03',
+            key: 3,
+            title: 'Produto 03',
             description: 'Descrição produto 03',
             value: 30,
             qtd: 1,
             valueTotal: 30
         },
         {
-            index: 4,
-            name: 'Produto 04',
+            key: 4,
+            title: 'Produto 04',
             description: 'Descrição produto 04',
             value: 40,
             qtd: 1,
             valueTotal: 40
         },
         {
-            index: 5,
-            name: 'Produto 05',
+            key: 5,
+            title: 'Produto 05',
             description: 'Descrição produto 05',
             value: 50,
             qtd: 1,
@@ -51,62 +90,26 @@ const Cart = ({ navigation }) => {
     return (
         <View style={styles.containerTopoTitle}>
             <Text style={styles.textTopoTitle}>Carrinho</Text>
-            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'space-between' }}>
-                <View>
-                    <ScrollView style={{ height: '100%', flex: 1 }}>
-                        {allProducts.map((product, index) => {
-                            return (
-                                <View style={{ marginBottom: 20 }}>
-                                    <View style={styles.productCard}>
-                                        <View style={styles.imagemProductCard}>
-                                            <Image
-                                                style={styles.imageCard}
-                                                source={require('../../assets/legumes_categorie.png')}
-                                            />
-                                        </View>
-                                        <View style={styles.containerTextProductCard}>
-                                            <Text style={styles.textProductCard}>
-                                                {product.description}
-                                            </Text>
-                                            <Text style={styles.textPriceProduct}>
-                                                R$ {product.value}
-                                            </Text>
-                                            <View style={styles.containerTotalQtd}>
-                                                <Text style={styles.textTotalQtd}>{product.qtd}</Text>
-                                            </View>
-                                            <View style={styles.containerNameProduct}>
-                                                <Text>{product.name}</Text>
-                                            </View>
-                                        </View>
-                                        <View style={styles.containerButtonAndValue}>
-                                            <View style={styles.buttonsRemoveProduct}>
-                                                <View>
-                                                    <Icon name="trash" size={25} color={'#e74c3c'} />
-                                                </View>
-                                            </View>
-                                            <View style={styles.containerValueTotalProduct}>
-                                                <Text style={styles.textValueTotalProduct}>R$ {product.valueTotal}</Text>
-                                            </View>
-                                        </View>
-                                    </View>
-                                </View>
-
-                            )
-                        })}
-                    </ScrollView>
-                    {/* <View style={styles.containerBottom}>
-                        <Text>AAAAAAAABCDE</Text>
-                    </View> */}
-                </View>
+            <ScrollView>
+                <FlatList
+                    maxHeight={500}
+                    data={allProducts}
+                    renderItem={( { item } ) => <CartProduct product={item} />}
+                />
             </ScrollView>
-            {/* <View style={styles.bottomView} >
-                <Text style={styles.textStyle}>This is Bottom View.</Text>
-            </View> */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'flex-start', paddingTop: 20 }}>
+                <View>
+                    <Button mode="contained">Comprar</Button>
+                </View>
+                <View>
+                    <Text style={{ fontSize: 25, fontWeight: 'bold'}}>Valor: 100,00</Text>
+                </View>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
     containerTopoTitle: {
         marginLeft: 30,
         marginTop: 20
@@ -200,6 +203,6 @@ const styles = StyleSheet.create({
     containerBottom: {
         backgroundColor: '#FF9800',
     }
-});
+} );
 
 export default Cart
