998fa04d6becd6db8101537525167290749ef461
gabrielgsv <gabriel.soares.vilela@gmail.com>
2019-09-12 20:02:29 -0300

    feat: crud and relationship cart

--- api/app/Models/User.js
+++ api/app/Models/User.js
@@ -17,6 +17,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  cart() {
+    return this.hasOne('App/Models/Cart')
+  }
 }
 
 module.exports = User
---
+++ api/database/migrations/1568327205044_cart_schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CartSchema extends Schema {
+  up() {
+    this.create('carts', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table
+        .integer('product_id')
+        .unsigned()
+        .references('id')
+        .inTable('products')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.integer('amount')
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('carts')
+  }
+}
+
+module.exports = CartSchema
