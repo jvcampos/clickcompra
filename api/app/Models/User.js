d08a94227b993228b2f3d3fc2e8a9465171301a8
gabrielgsv <gabriel.soares.vilela@gmail.com>
2019-09-12 20:11:13 -0300

    revert

--- api/app/Models/User.js
+++ api/app/Models/User.js
@@ -17,10 +17,6 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
-
-  cart() {
-    return this.hasOne('App/Models/Cart')
-  }
 }
 
 module.exports = User
